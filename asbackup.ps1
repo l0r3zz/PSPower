4082551f938a68f5bd519648ab7d5108928c8117
Geoff White <gwhite@geoffw-SH67H3.(none)>
2011-12-27 14:12:41 -0800

    added usage message and named parameters (only -backup is functional)

--- asbackup.ps1
+++ asbackup.ps1
@@ -12,19 +12,28 @@
 #
 #
 # =============================================================================
+[CmdletBinding()]
+Param (
+    [switch]$backup,
+    [switch]$restore,
+    [switch]$noprivdata,
+#    [Parameter(Mandatory=$True)]
+    [string[]]$bundle
+)
 
-Function asbackup {
-    [CmdletBinding()]
-    Param (
-            [switch]$backup,
-            [switch]$restore,
-            [switch]$noprivdata,
-            [Parameter(Mandatory=$True)]
-            [string[]]
-            $bundle
-    )
-            
-    cd 'C:\Program Files (x86)\Aspera\Enterprise Server\etc'
-    get-childitem * -inc aspera.conf,passwd,ui.conf,sync-conf.xml,docroot,group`| 
-    write-tar -output "c:\Windows\Temp\$bundle" + ".tar"
+#$debugPreference = "Continue"
+if ($backup.isPresent){
+    $homepath = pwd
+    $asperaetc = "C:\Program Files (x86)\Aspera\Enterprise Server\etc"  
+    $bundlepath =   "c:\Windows\Temp\$bundle" + ".tar"
+    $BackupList = "aspera.conf,passwd,ui.conf,sync-conf.xml,docroot,group"   
+    Set-Location $asperaetc|out-null
+    get-childitem * -inc aspera.conf,passwd,ui.conf,sync-conf.xml,docroot,group|
+#    get-childitem * -inc $backupList | 
+    write-tar -output $bundlepath | out-null
+    Set-location $homepath |out-null
+}elseif( $restore.isPresent){
+    Write-Host "Not implemented"
+}else{
+    Write-Host "Usage: asbackup -backup [-noprivdata] | -restore <bundle>"
 }
