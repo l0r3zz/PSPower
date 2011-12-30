19ae592dc6f6829dc0fb0fe900a3dccb9d22ef85
unknown <Administrator@geoffw-es.gwsupport.asperasoft.com>
2011-12-30 14:05:49 -0800

    write a .asmanifest file containing metadata currently contains the asperaetc path

--- asbackup.ps1
+++ asbackup.ps1
@@ -23,18 +23,38 @@ Import-Module Pscx       #Use the PowerShell Community Extensions
 #$debugPreference = "Continue"
 
 if ($backup.isPresent){
+	# Some initisl variables 
     $homepath = pwd
     $asperaetc = "C:\Program Files (x86)\Aspera\Enterprise Server\etc"
 	$asperaetcfiles = $asperaetc + "\*"
-    $bundlepath =   "c:\Windows\Temp\$bundle" + ".tar"
+    $bundlepath =   "c:\Windows\Temp\"
+	$bundlefile = $bundlepath + "$bundle.zip"
+	$manifestfile = ".asmanifest.txt"
     $BackupList = "aspera.conf","passwd","ui.conf","sync-conf.xml",
-				  "docroot","group"   
+				  "docroot","group"
+				  
+	# CD over to the aspera /etc directory
     Set-Location $asperaetc|out-null
+	
+	# Get the list of context files and write them to a zip archive
     get-childitem $asperaetcfiles -force -inc $BackupList |
-    write-tar -output $bundlepath | out-null
+    write-Zip -output $bundlefile | out-null
+	
+	# Create a manifet file and append it to the zip archive
+	New-Item -Path $bundlepath -Name $manifestfile -type "file" `
+	    -Value "bundlepath:$bundlepath"
+	Write-Zip -Path ($bundlepath + $manifestfile) -Append `
+	    -OutputPath $bundlefile 
+		
+	# Remove the manifest file
+	Remove-Item -Path ($bundlepath + $manifestfile) |out-null
+	
+	# CD back to where you started
     Set-location $homepath |out-null
+	
 }elseif( $restore.isPresent){
     Write-Host "Not implemented"
+	
 }else{
     Write-Host "Usage: asbackup -backup [-noprivdata] | -restore <bundle>"
 }
